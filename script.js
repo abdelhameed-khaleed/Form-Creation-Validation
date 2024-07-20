6a017d0bea82285d1a54d51279d833d2b3daa251
Abdelhameed <am97130811@yahoo.com>
2024-07-20 10:33:14 +0300

    adding code inside event listner

--- script.js
+++ script.js
@@ -1,56 +1,54 @@
 
+document.addEventListener('DOMContentLoaded', function() {
+
 // Setup and Initial Code Structure
 
 const form =document.getElementById('registration-form');
 const feedbackDiv =document.getElementById('form-feedback');
 
 // Form Submission and Event Prevention
-form.addEventListener('click',(event)=>{
-    event.preventDefault()
-})
+form.addEventListener('submit',(event)=>{
+    event.preventDefault();
+
+    // Input Retrieval and Trimming
+    let username = document.getElementById('username').value.trim();
+    let email = document.getElementById('email').value.trim() ;
+    let password = document.getElementById('password').value.trim() ;
 
 
-// Input Retrieval and Trimming
+    // Validation Logic
+    let isValid = true;
+    let messages =[]
 
-let username = document.getElementById('username') ;
-username = username.value.trim()
+    if(username.length < 3 ){
+        isValid = false;
+        messages.push('UserName has to be > 3digits  ')
+    }
 
-let email = document.getElementById('email') ;
-email = email.value.trim()
+    if( !email.includes('@') || !email.includes('.')){
+        isValid = false;
+        messages.push('Please Include Valid email with @ and .')
+    }
 
-let passowrd = document.getElementById('passowrd') ;
-passowrd = passowrd.value.trim()
-
-
-// Validation Logic
-const isValid = true;
-let messages =[]
-
-if(username.length < 3 ){
-    isValid = false;
-    messages.push('UserName has to be > 3digits  ')
-}
-
-if(email.includes('@') && email.includes('.')){
-    isValid = false;
-    messages.push('Please Include Valid email with @ and .')
-}
-
-if(password.length < 8 ){
-    isValid = false;
-    messages.push('Password has to be > 7 digits  ')
-}
+    if(password.length < 8 ){
+        isValid = false;
+        messages.push('Password has to be > 7 digits  ')
+    };
 
 // Displaying Feedback
 
-feedbackDiv.style.display = 'block';
+    feedbackDiv.style.display = 'block';
 
-if (isValid){
-    feedbackDiv.textContent = 'Registration successful!'
-    feedbackDiv.style.color ="#28a745";
-}
-else{
-    feedbackDiv.innerHTML=messages.join("<br>");
-    feedbackDiv.style.color ="#dc3545";
+    if (isValid){
+        feedbackDiv.textContent = 'Registration successful!'
+        feedbackDiv.style.color ="#28a745";
+    }
+    else{
+        feedbackDiv.innerHTML=messages.join("<br>");
+        feedbackDiv.style.color ="#dc3545";
 
-}
+    }
+
+});
+
+});
